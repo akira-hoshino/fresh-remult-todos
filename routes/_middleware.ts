09ebb787019a7e7191d845e34c5b22b9f222ff1e
HOSHINO <akira.hoshino@v-crews.co.jp>
2022-11-05 17:28:01 +0900

    Connect to postgres on supabase

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,8 +1,17 @@
 import { remultFresh } from "remult/remult-fresh";
 import { Task } from "../model/task.ts"
+import { createPostgresConnection } from "https://deno.land/x/remult/postgres.ts";
+import "https://deno.land/x/dotenv/load.ts";
 
 export const remultServer = remultFresh({
-  entities: [Task]
+  entities: [Task],
+  dataProvider: async () => {
+    const dbUrl = Deno.env.get("DATABASE_URL");
+    if (dbUrl) {
+      return createPostgresConnection({ connectionString: dbUrl });
+    }
+    return await undefined;
+  },
 }, Response);
 
 export const handler = remultServer.handle;
